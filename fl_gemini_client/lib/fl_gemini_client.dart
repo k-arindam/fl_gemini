5a6d82bd474b0e2ac57dc0644f52df5dfabdb70a
Arindam Karmakar <arindamk410@gmail.com>
2023-12-20 01:33:46 +0530

    Added exports

--- fl_gemini_client/lib/fl_gemini_client.dart
+++ fl_gemini_client/lib/fl_gemini_client.dart
@@ -1,3 +1,11 @@
 library fl_gemini_client;
 
 export 'package:fl_gemini_client/src/client/client.dart';
+
+export 'package:fl_gemini_client/src/enums/gemini_model.dart';
+export 'package:fl_gemini_client/src/enums/message_entity.dart';
+export 'package:fl_gemini_client/src/enums/message_type.dart';
+
+export 'package:fl_gemini_client/src/models/gemini_message.dart';
+
+export 'package:fl_gemini_client/src/extensions/string_extension.dart';
